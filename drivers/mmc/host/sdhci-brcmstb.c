3a78e0f7a23ec00e7cb4dbf107fa558c57a57240
Jaedon Shin <jaedon.shin@gmail.com>
2014-12-18 10:51:08 +0900

    mmc: sdhci-brcmstb: add a sdio host driver for bcm7xxx platforms
    Add a host driver for ARM based set-top box platforms.
    
    Signed-off-by: Jaedon Shin <jaedon.shin@gmail.com>

---
+++ drivers/mmc/host/sdhci-brcmstb.c
@@ -0,0 +1,121 @@
+/*
+ * sdhci-brcmstb.c Support for SDHCI on Broadcom SoC's
+ *
+ * Copyright (C) 2013 Broadcom Corporation
+ *
+ * Author: Al Cooper <acooper@broadcom.com>
+ * Based on sdhci-dove.c
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <linux/io.h>
+#include <linux/mmc/host.h>
+#include <linux/module.h>
+#include <linux/of.h>
+
+#include "sdhci-pltfm.h"
+
+
+static struct sdhci_pltfm_data sdhci_brcmstb_pdata = {
+};
+
+#ifdef CONFIG_PM_SLEEP
+
+static int sdhci_brcmstb_suspend(struct device *dev)
+{
+	struct sdhci_host *host = dev_get_drvdata(dev);
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	int res;
+
+	res = sdhci_suspend_host(host);
+	if (res)
+		return res;
+	clk_disable(pltfm_host->clk);
+	return res;
+}
+
+static int sdhci_brcmstb_resume(struct device *dev)
+{
+	struct sdhci_host *host = dev_get_drvdata(dev);
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	int err;
+
+	err = clk_enable(pltfm_host->clk);
+	if (err)
+		return err;
+	return sdhci_resume_host(host);
+}
+
+#endif /* CONFIG_PM_SLEEP */
+
+static SIMPLE_DEV_PM_OPS(sdhci_brcmstb_pmops, sdhci_brcmstb_suspend,
+			sdhci_brcmstb_resume);
+
+static int sdhci_brcmstb_probe(struct platform_device *pdev)
+{
+	struct device_node *dn = pdev->dev.of_node;
+	struct sdhci_host *host;
+	struct sdhci_pltfm_host *pltfm_host;
+	int res;
+
+	res = sdhci_pltfm_register(pdev, &sdhci_brcmstb_pdata, 0);
+	if (res)
+		return res;
+	host = platform_get_drvdata(pdev);
+	pltfm_host = sdhci_priv(host);
+
+	pltfm_host->clk = of_clk_get_by_name(dn, "sw_sdio");
+	if (IS_ERR(pltfm_host->clk)) {
+		dev_err(&pdev->dev, "Clock not found in Device Tree\n");
+		pltfm_host->clk = NULL;
+	}
+	res = clk_prepare_enable(pltfm_host->clk);
+	if (res)
+		sdhci_pltfm_unregister(pdev);
+	return res;
+}
+
+static int sdhci_brcmstb_remove(struct platform_device *pdev)
+{
+	struct sdhci_host *host = platform_get_drvdata(pdev);
+	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
+	int res;
+	res = sdhci_pltfm_unregister(pdev);
+	clk_disable_unprepare(pltfm_host->clk);
+	return res;
+}
+
+
+static const struct of_device_id sdhci_brcm_of_match[] = {
+	{ .compatible = "brcm,sdhci-brcmstb" },
+	{},
+};
+
+static struct platform_driver sdhci_brcmstb_driver = {
+	.driver		= {
+		.name	= "sdhci-brcmstb",
+		.owner	= THIS_MODULE,
+		.pm	= &sdhci_brcmstb_pmops,
+		.of_match_table = of_match_ptr(sdhci_brcm_of_match),
+	},
+	.probe		= sdhci_brcmstb_probe,
+	.remove		= sdhci_brcmstb_remove,
+};
+
+module_platform_driver(sdhci_brcmstb_driver);
+
+MODULE_DESCRIPTION("SDHCI driver for Broadcom");
+MODULE_AUTHOR("Al Cooper <acooper@broadcom.com>");
+MODULE_LICENSE("GPL v2");
