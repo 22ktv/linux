b49fac1c7f400f06169dc4cd441775b922f22409
Jaedon Shin <jaedon.shin@gmail.com>
2015-06-02 09:45:47 +0900

    Revert "media: dmxdev: fix possible race condition"
    This reverts commit 5728f6600617b5616795bb6ef455839fc7959815.
    
    Signed-off-by: Jaedon Shin <jaedon.shin@gmail.com>

--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -57,11 +57,10 @@ static int dvb_dmxdev_buffer_write(struct dvb_ringbuffer *buf,
 	return dvb_ringbuffer_write(buf, src, len);
 }
 
-static ssize_t dvb_dmxdev_buffer_read(struct dmxdev_filter *dmxdevfilter,
+static ssize_t dvb_dmxdev_buffer_read(struct dvb_ringbuffer *src,
 				      int non_blocking, char __user *buf,
 				      size_t count, loff_t *ppos)
 {
-	struct dvb_ringbuffer *src = &dmxdevfilter->buffer;
 	size_t todo;
 	ssize_t avail;
 	ssize_t ret = 0;
@@ -76,21 +75,16 @@ static ssize_t dvb_dmxdev_buffer_read(struct dmxdev_filter *dmxdevfilter,
 	}
 
 	for (todo = count; todo > 0; todo -= ret) {
-		if (dvb_ringbuffer_empty(src)) {
-			mutex_unlock(&dmxdevfilter->mutex);
-
-			if (non_blocking)
-				return -EWOULDBLOCK;
-
-			ret = wait_event_interruptible(src->queue,
-					!dvb_ringbuffer_empty(src) ||
-					(src->error != 0));
-			if (ret < 0)
-				return ret;
-
-			if (mutex_lock_interruptible(&dmxdevfilter->mutex))
-				return -ERESTARTSYS;
+		if (non_blocking && dvb_ringbuffer_empty(src)) {
+			ret = -EWOULDBLOCK;
+			break;
 		}
+
+		ret = wait_event_interruptible(src->queue,
+					       !dvb_ringbuffer_empty(src) ||
+					       (src->error != 0));
+		if (ret < 0)
+			break;
 
 		if (src->error) {
 			ret = src->error;
@@ -248,63 +242,13 @@ static ssize_t dvb_dvr_read(struct file *file, char __user *buf, size_t count,
 {
 	struct dvb_device *dvbdev = file->private_data;
 	struct dmxdev *dmxdev = dvbdev->priv;
-	struct dvb_ringbuffer *src = &dmxdev->dvr_buffer;
-	size_t todo;
-	ssize_t avail;
-	ssize_t ret = 0;
 
-	if (mutex_lock_interruptible(&dmxdev->mutex))
-		return -ERESTARTSYS;
-
-	if (dmxdev->exit) {
-		mutex_unlock(&dmxdev->mutex);
+	if (dmxdev->exit)
 		return -ENODEV;
-	}
 
-	if (src->error) {
-		ret = src->error;
-		dvb_ringbuffer_flush(src);
-		mutex_unlock(&dmxdev->mutex);
-		return ret;
-	}
-
-	for (todo = count; todo > 0; todo -= ret) {
-		if (dvb_ringbuffer_empty(src)) {
-			mutex_unlock(&dmxdev->mutex);
-
-			if (file->f_flags & O_NONBLOCK)
-				return -EWOULDBLOCK;
-
-			ret = wait_event_interruptible(src->queue,
-					!dvb_ringbuffer_empty(src) ||
-					(src->error != 0));
-			if (ret < 0)
-				return ret;
-
-			if (mutex_lock_interruptible(&dmxdev->mutex))
-				return -ERESTARTSYS;
-		}
-
-		if (src->error) {
-			ret = src->error;
-			dvb_ringbuffer_flush(src);
-			break;
-		}
-
-		avail = dvb_ringbuffer_avail(src);
-		if (avail > todo)
-			avail = todo;
-
-		ret = dvb_ringbuffer_read_user(src, buf, avail);
-		if (ret < 0)
-			break;
-
-		buf += ret;
-	}
-
-	mutex_unlock(&dmxdev->mutex);
-
-	return (count - todo) ? (count - todo) : ret;
+	return dvb_dmxdev_buffer_read(&dmxdev->dvr_buffer,
+				      file->f_flags & O_NONBLOCK,
+				      buf, count, ppos);
 }
 
 static int dvb_dvr_set_buffer_size(struct dmxdev *dmxdev,
@@ -339,6 +283,7 @@ static int dvb_dvr_set_buffer_size(struct dmxdev *dmxdev,
 
 	return 0;
 }
+
 static inline void dvb_dmxdev_filter_state_set(struct dmxdev_filter
 					       *dmxdevfilter, int state)
 {
@@ -959,7 +904,7 @@ static ssize_t dvb_dmxdev_read_sec(struct dmxdev_filter *dfil,
 		hcount = 3 + dfil->todo;
 		if (hcount > count)
 			hcount = count;
-		result = dvb_dmxdev_buffer_read(dfil,
+		result = dvb_dmxdev_buffer_read(&dfil->buffer,
 						file->f_flags & O_NONBLOCK,
 						buf, hcount, ppos);
 		if (result < 0) {
@@ -980,7 +925,7 @@ static ssize_t dvb_dmxdev_read_sec(struct dmxdev_filter *dfil,
 	}
 	if (count > dfil->todo)
 		count = dfil->todo;
-	result = dvb_dmxdev_buffer_read(dfil,
+	result = dvb_dmxdev_buffer_read(&dfil->buffer,
 					file->f_flags & O_NONBLOCK,
 					buf, count, ppos);
 	if (result < 0)
@@ -1002,12 +947,11 @@ dvb_demux_read(struct file *file, char __user *buf, size_t count,
 	if (dmxdevfilter->type == DMXDEV_TYPE_SEC)
 		ret = dvb_dmxdev_read_sec(dmxdevfilter, file, buf, count, ppos);
 	else
-		ret = dvb_dmxdev_buffer_read(dmxdevfilter,
+		ret = dvb_dmxdev_buffer_read(&dmxdevfilter->buffer,
 					     file->f_flags & O_NONBLOCK,
 					     buf, count, ppos);
 
-	if (mutex_is_locked(&dmxdevfilter->mutex))
-		mutex_unlock(&dmxdevfilter->mutex);
+	mutex_unlock(&dmxdevfilter->mutex);
 	return ret;
 }
 
