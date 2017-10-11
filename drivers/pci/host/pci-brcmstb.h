d5ed146e087a258db6450b1cd9ac2bcd556227fd
Jim Quinlan <jim2101024@gmail.com>
2017-10-11 18:34:23 -0400

    PCI: host: brcmstb: Broadcom PCIe Host Controller
    This commit adds the basic Broadcom STB PCIe controller.  Missing is
    the ability to process MSI and also handle dma-ranges for inbound
    memory accesses.  These two functionalities are added in subsequent
    commits.
    
    The PCIe block contains an MDIO interface.  This is a local interface
    only accessible by the PCIe controller.  It cannot be used or shared
    by any other HW.  As such, the small amount of code for this
    controller is included in this driver as there is little upside to put
    it elsewhere.
    
    Signed-off-by: Jim Quinlan <jim2101024@gmail.com>

---
+++ drivers/pci/host/pci-brcmstb.h
@@ -0,0 +1,33 @@
+#ifndef __BRCMSTB_PCI_H
+#define __BRCMSTB_PCI_H
+
+/*
+ * Copyright (C) 2015 - 2017 Broadcom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#define BRCM_PCIE_NAME_SIZE		8
+#define BRCM_INT_PCI_MSI_NR		32
+#define BRCM_PCIE_HW_REV_33		0x0303
+
+/* Broadcom PCIE Offsets */
+#define PCIE_INTR2_CPU_BASE		0x4300
+
+#if defined(CONFIG_MIPS)
+/* Broadcom MIPs HW implicitly does the swapping if necessary */
+#define bcm_readl(a)		__raw_readl(a)
+#define bcm_writel(d, a)	__raw_writel(d, a)
+#else
+#define bcm_readl(a)		readl(a)
+#define bcm_writel(d, a)	writel(d, a)
+#endif
+
+#endif /* __BRCMSTB_PCI_H */
