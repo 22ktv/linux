5e011c6ecc4dce46152e4acfd11b695b501bfd2b
Jaedon Shin <jaedon.shin@gmail.com>
2017-11-29 11:10:24 +0900

    [media] dvb: Revert get_caps() and set_source() remove
    Signed-off-by: Jaedon Shin <jaedon.shin@gmail.com>

--- drivers/media/dvb-core/demux.h
+++ drivers/media/dvb-core/demux.h
@@ -576,6 +576,8 @@ struct dmx_demux {
 	int (*get_pes_pids)(struct dmx_demux *demux, u16 *pids);
 
 	/* private: */
+	int (*get_caps)(struct dmx_demux *demux, struct dmx_caps *caps);
+	int (*set_source)(struct dmx_demux *demux, const enum dmx_source *src);
 
 	/*
 	 * Only used at av7110, to read some data from firmware.
--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -1043,6 +1043,22 @@ static int dvb_demux_do_ioctl(struct file *file,
 		dmxdev->demux->get_pes_pids(dmxdev->demux, parg);
 		break;
 
+	case DMX_GET_CAPS:
+		if (!dmxdev->demux->get_caps) {
+			ret = -EINVAL;
+			break;
+		}
+		ret = dmxdev->demux->get_caps(dmxdev->demux, parg);
+		break;
+
+	case DMX_SET_SOURCE:
+		if (!dmxdev->demux->set_source) {
+			ret = -EINVAL;
+			break;
+		}
+		ret = dmxdev->demux->set_source(dmxdev->demux, parg);
+		break;
+
 	case DMX_GET_STC:
 		if (!dmxdev->demux->get_stc) {
 			ret = -EINVAL;
