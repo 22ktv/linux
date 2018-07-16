701ef2827376dc709fa102128a3a5aa69ca3892a
Jaedon Shin <jaedon.shin@gmail.com>
2018-07-16 13:58:09 +0900

    Revert "irqchip/gic: Loudly complain about the use of IRQ_TYPE_NONE"
    This reverts commit 83a86fbb5b56b5eed8a476cc3fe214077d7c4f49.

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -1016,9 +1016,6 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 			*hwirq += 16;
 
 		*type = fwspec->param[2] & IRQ_TYPE_SENSE_MASK;
-
-		/* Make it clear that broken DTs are... broken */
-		WARN_ON(*type == IRQ_TYPE_NONE);
 		return 0;
 	}
 
@@ -1028,8 +1025,6 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 
 		*hwirq = fwspec->param[0];
 		*type = fwspec->param[1];
-
-		WARN_ON(*type == IRQ_TYPE_NONE);
 		return 0;
 	}
 
