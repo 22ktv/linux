d1763d9ea66574cda3b48b2b8cb0e61b4299798a
Jaedon Shin <jaedon.shin@gmail.com>
2018-11-24 00:06:15 +0900

    Subject: [PATCH 7/7] [cfg80211]: fixing dfs master issue
    From 1c3026f52d439f1933d4d89c81b7c7647171e963 Mon Sep 17 00:00:00 2001
    From: Debabrat Das <c-debabr@codeaurora.org>
    Date: Fri, 27 Jul 2018 20:44:32 +0530
    Subject: [PATCH 7/7] [cfg80211]: fixing dfs master issue
    
    removing wrong dfs checks and adding correct
    condition.

--- net/wireless/chan.c
+++ net/wireless/chan.c
@@ -479,8 +479,7 @@ static int cfg80211_get_chans_dfs_required(struct wiphy *wiphy,
 		if (!c)
 			return -EINVAL;
 
-		if ((c->flags & IEEE80211_CHAN_RADAR) &&
-		    !(wiphy->flags & WIPHY_FLAG_DFS_OFFLOAD))
+		if (c->flags & IEEE80211_CHAN_RADAR)
 			return 1;
 	}
 	return 0;
@@ -761,8 +760,7 @@ static bool cfg80211_get_chans_dfs_available(struct wiphy *wiphy,
 		if (c->flags & IEEE80211_CHAN_DISABLED)
 			return false;
 
-		if (!(wiphy->flags & WIPHY_FLAG_DFS_OFFLOAD) &&
-		    (c->flags & IEEE80211_CHAN_RADAR) &&
+		if ((c->flags & IEEE80211_CHAN_RADAR) &&
 		    (c->dfs_state != NL80211_DFS_AVAILABLE) &&
 		    !(c->dfs_state == NL80211_DFS_USABLE && dfs_offload))
 			return false;
@@ -876,7 +874,8 @@ static bool cfg80211_secondary_chans_ok(struct wiphy *wiphy,
 
 	for (freq = start_freq; freq <= end_freq; freq += MHZ_TO_KHZ(20)) {
 		c = ieee80211_get_channel_khz(wiphy, freq);
-		if (!c || c->flags & prohibited_flags)
+		if (!c || (c->flags & prohibited_flags &&
+		    !(wiphy->flags & WIPHY_FLAG_DFS_OFFLOAD)))
 			return false;
 	}
 
