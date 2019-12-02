3d9e4fce6bd9a6f5b7e8b49b546b4ecfeb95b29e
Jaedon Shin <jaedon.shin@gmail.com>
2019-12-02 15:12:57 +0900

    Revert "vfs: don't allow writes to swap files"
    This reverts commit dc617f29dbe5ef0c8ced65ce62c464af1daaab3d.

--- mm/filemap.c
+++ mm/filemap.c
@@ -2963,9 +2963,6 @@ inline ssize_t generic_write_checks(struct kiocb *iocb, struct iov_iter *from)
 	loff_t count;
 	int ret;
 
-	if (IS_SWAPFILE(inode))
-		return -ETXTBSY;
-
 	if (!iov_iter_count(from))
 		return 0;
 
